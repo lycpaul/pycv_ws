44f0d9062b3101781e802448b41fc3f3e417490a
lycpaul <p.lee@precisionrobotics.com>
2020-07-15 23:33:17 +0800

    remove py cache

--- gst_pipeline/opencv_gst_test.py
+++ gst_pipeline/opencv_gst_test.py
@@ -16,7 +16,12 @@ gst-launch-1.0 udpsrc port=5000 ! application/x-rtp, encoding-name=H264,payload=
 
 # QString GST_PIPELINE_RECORDING = "v4l2src device=%1 ! timeoverlay ypad=5 xpad=5 auto-resize=false ! video/x-raw, width=1920, height=1080, framerate=60/1 ! videoconvert ! tee name=t ! queue ! vaapivp9enc ! webmmux ! filesink location=%2 t. ! videocrop left=168 right=168 ! videoscale ! video/x-raw, width=1760, height=1200 ! queue leaky=1 ! videoconvert ! appsink drop = true";
 
-cap = cv2.VideoCapture("v4l2src device=/dev/video0 ! video/x-raw, width=1920, height=1080, framerate=60/1 ! tee name=t ! queue ! xvimagesink t. ! videoconvert ! queue leaky=1 ! videoconvert ! appsink drop = true", cv2.CAP_GSTREAMER)
+# cap = cv2.VideoCapture("v4l2src device=/dev/video0 ! video/x-raw, width=1920, height=1080, framerate=60/1 ! tee name=t ! queue ! xvimagesink t. ! videoconvert ! queue leaky=1 ! videoconvert ! appsink drop = true", cv2.CAP_GSTREAMER)
+
+cap = cv2.VideoCapture("videotestsrc pattern=ball ! video/x-raw, width=1920, height=1080, framerate=60/1 ! tee name=t ! queue ! xvimagesink t. ! videoconvert ! queue leaky=1 ! videoconvert ! appsink drop = true", cv2.CAP_GSTREAMER)
+
+import time
+t_0 = time.time()
 
 while True:
     ret,frame = cap.read()
@@ -25,6 +30,5 @@ while True:
         continue    
     cv2.imshow('receive', frame)
     if cv2.waitKey(1)&0xFF == ord('q'):
-        break    
+        break
 cap.release()
-cap.receive()
